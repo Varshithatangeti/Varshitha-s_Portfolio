d4a11b8fd6eb9f690d0fc96495f5eeca0de7b18b
Varshitha Tangeti <142086561+Varshithatangeti@users.noreply.github.com>
2025-08-31 16:47:55 +0530

    Delete backend directory

--- backend/controllers/contactController.js
+++
@@ -1,5 +0,0 @@
-exports.handleContactForm = (req, res) => {
-    const { name, email, message } = req.body;
-    console.log("Contact Received:", { name, email, message });
-    res.status(200).json({ message: 'Contact form submitted successfully' });
-};
--- backend/routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleContactForm } = require('../controllers/contactController');
-
-router.post('/', handleContactForm);
-
-module.exports = router;
